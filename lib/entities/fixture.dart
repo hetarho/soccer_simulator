3c38b182fa1433abbc69f761b3fe7ba72de81463
hetarho <sunlikeperson@gmail.com>
2024-03-13 00:17:03 +0900

    [MODIFY] fixture 규칙 추가

--- lib/entities/fixture.dart
+++ lib/entities/fixture.dart
@@ -3,7 +3,8 @@ import 'package:soccer_simulator/entities/club.dart';
 class Fixture {
   final Club homeClub;
   final Club awayClub;
-  bool isplayed = false;
+
+  int playTime = 0;
   int homeTeamBallPercentage = 100;
 
   Fixture({required this.homeClub, required this.awayClub});
