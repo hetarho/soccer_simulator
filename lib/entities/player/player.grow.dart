b854091039e0aa0d1deaf58de6737d7f9369f2f0
hetarho <sunlikeperson@gmail.com>
2024-05-13 20:32:49 +0900

    [ADD]성장 로직 복구

--- lib/entities/player/player.grow.dart
+++ lib/entities/player/player.grow.dart
@@ -47,7 +47,6 @@ extension PlayerGrow on Player {
 
   ///실제 경기를 뛰면서 발생하는 스텟 성장 - 출전 포지션에 따라 다르게 성장
   void _growAfterPlay() {
-    return;
     //남은 포텐셜이 0보다 커야 성장 가능, 30이상이면 경기시마다 항상 성장
     if (_potential / 30 > Random().nextDouble()) {
       if (Random().nextDouble() > 0.3) {
@@ -55,13 +54,15 @@ extension PlayerGrow on Player {
         Stat newStat = Stat.playGame(role: role, point: R().getInt(max: R().getDouble(max: 1) > 0.8 ? 1 : 0));
         _stat.add(newStat);
       }
-    } else {
-      ///포텐이 떨어졌는데 나이가 많아서 에이징커브가 올 경우
-      if (age - 25 > R().getInt(max: 15)) {
-        Stat newStat = Stat.agingCurve(point: R().getInt(min: -1 * age ~/ 10, max: 0));
-        _stat.add(newStat);
-      }
     }
+
+    ///포텐이 떨어졌는데 나이가 많아서 에이징커브가 올 경우
+    // else {
+    //   if (age - 25 > R().getInt(max: 15)) {
+    //     Stat newStat = Stat.agingCurve(point: R().getInt(min: -1 * age ~/ 10, max: 0));
+    //     _stat.add(newStat);
+    //   }
+    // }
   }
 
   Position _getWantedPositionFromStat(Stat stat) {
