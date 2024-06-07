7e7751c9b1951f9cb186619943009a2996543563
hetarho <sunlikeperson@gmail.com>
2024-06-07 19:26:14 +0900

    [ADD] create page 초기작업

--- lib/ui/pages/create/create_save_slot_page.dart
+++ lib/ui/pages/create/create_save_slot_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:soccer_simulator/data/data_source/save_slot_local_data_source.dart';
+import 'package:soccer_simulator/data/repositories/interfaces/save_slot_data_source.dart';
+import 'package:soccer_simulator/data/repositories/save_slot_repository.dart';
+import 'package:soccer_simulator/ui/dbManager/db_manager.dart';
 import 'package:soccer_simulator/ui/providers/providers.dart';
 import 'package:soccer_simulator/ui/providers/vo/create_save_slot_vo.dart';
 
@@ -12,6 +16,19 @@ class CreateSaveSlotPage extends ConsumerStatefulWidget {
 }
 
 class _CreatePageState extends ConsumerState<CreateSaveSlotPage> {
+  _create() async {
+    await DbManager.init();
+
+    ///dbManager 객체 생성
+    DbManager dbManager = DbManager();
+
+    ///data source 의존성 주입을 통해 객체 생성
+    SaveSlotDataSource saveSlotDataSource = SaveSlotLocalDataSource(dbManager);
+
+    /// repository 의존성 주입을 통해 객체 생성
+    SaveSlotRepository saveSlotRepository = SaveSlotRepository(saveSlotDataSource);
+  }
+
   @override
   Widget build(BuildContext context) {
     CreateSaveSlotVo createSaveSlotVo = ref.read(createSaveSlotProvider);
@@ -23,6 +40,7 @@ class _CreatePageState extends ConsumerState<CreateSaveSlotPage> {
           Text(createSaveSlotVo.selectedLeagueSeed?.name ?? ''),
           Text('club'),
           Text(createSaveSlotVo.selectedClubSeed?.name ?? ''),
+          ElevatedButton(onPressed: _create, child: Text('create')),
         ],
       ),
     );
